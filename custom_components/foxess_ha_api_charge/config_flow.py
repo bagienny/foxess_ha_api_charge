76f0460f0ee32c63f215a1187d539968e0dfa030
bagienny <118523767+bagienny@users.noreply.github.com>
2026-03-14 21:54:30 +0100

    Update config_flow.py
    unified name and code cleanup

--- custom_components/foxess_ha_api_charge/config_flow.py
+++ custom_components/foxess_ha_api_charge/config_flow.py
@@ -8,7 +8,6 @@ STEP_USER_DATA_SCHEMA = vol.Schema({
 })
 
 class FoxESSChargeScheduleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Config flow for FoxESS Charge Schedule."""
 
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
@@ -18,7 +17,7 @@ class FoxESSChargeScheduleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         if user_input is not None:
             return self.async_create_entry(
-                title="FoxESS Charge Schedule", data=user_input
+                title="FoxESS Charge Periods", data=user_input
             )
 
         return self.async_show_form(step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors)
